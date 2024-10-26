85c56ccd69b1ff9e0e8b0018aa11c13006fd99e3
UCUgllekk <romanpavlosiuk10@gmail.com>
2024-10-26 22:47:48 +0300

    tests

--- src/main/java/ua/edu/ucu/apps/flowerstore/flower/Order.java
+++ src/main/java/ua/edu/ucu/apps/flowerstore/flower/Order.java
@@ -2,9 +2,12 @@ package ua.edu.ucu.apps.flowerstore.flower;
 
 import java.util.LinkedList;
 
+import lombok.Getter;
+import lombok.Setter;
 import ua.edu.ucu.apps.flowerstore.flower.Delivery.Delivery;
 import ua.edu.ucu.apps.flowerstore.flower.Payment.Payment;
 
+@Getter @Setter
 public class Order {
     private LinkedList<Item> items;
     private Payment payment;
